7ec8077f8fa3314d3890335fc2a5dfbbd3f84a1a
dinuchriist <lahiruzone@gmail.com>
2020-03-16 16:37:51 +0530

    chat model

---
+++ lib/models/chat_model.dart
@@ -0,0 +1,38 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+class Chat {
+  final String id;
+  final String name;
+  final String imageUrl;
+  final String recentMessage;
+  final String recentSender;
+  final Timestamp recentTimeStamp;
+  final List<dynamic> memberIds;
+  final dynamic memberInfor;
+  final dynamic readStatus;
+
+  Chat(
+      {this.id,
+      this.name,
+      this.imageUrl,
+      this.recentMessage,
+      this.recentSender,
+      this.recentTimeStamp,
+      this.memberIds,
+      this.memberInfor,
+      this.readStatus});
+
+  factory Chat.fromDoc(DocumentSnapshot doc) {
+    return Chat(
+      id: doc.documentID,
+      name: doc['name'],
+      imageUrl: doc['imageUrl'],
+      recentMessage: doc['recentMessage'],
+      recentSender: doc['recentSender'],
+      recentTimeStamp: doc['recentTimeStamp'],
+      memberIds: doc['memberIds'],
+      memberInfor: doc['memberInfo'],
+      readStatus: doc['readStatus'],
+    );
+  }
+}
